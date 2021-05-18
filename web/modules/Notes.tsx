9b1cd43b165ae0eb7d1a2c14f298a2c0c942f322
Jonathan Stephens <sirbrialliance@gmail.com>
2021-05-17 18:27:22 -0600

    fixup Notes

--- web/modules/Notes.tsx
+++ web/modules/Notes.tsx
@@ -2,16 +2,22 @@ import Module from './Module';
 
 export default class Notes extends Module {
 	getId() { return "Notes" }
-	getName(): string {
-		return "Notes";
-	}
+	getName() { return "Notes" }
+	renderThumb() { return <span>Just jot something down for later, data is saved in your browser locally.</span> }
 
-	renderThumb(): HTMLElement {
-		var el = <textarea onInput={ev => localStorage.setItem("notes", (ev.target as HTMLInputElement).value)}>
+
+	render() {
+		let el = <textarea onInput={ev => localStorage.setItem("notes", (ev.target as HTMLInputElement).value)}>
 			{localStorage.getItem("notes") || ""}
-		</textarea>;
+		</textarea>
 
-		return el;
+		return [
+			<p>
+				Write down whatever you'd like, data is stored locally via your browser's localStorage.
+				It should remain if you reload the page or restart the browser. Data isn't sent to the server.
+			</p>,
+			el
+		]
 	}
 
 }
